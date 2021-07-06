06fc6401b20123babb9ae734d640bda964c7e208
Commodoreprime <35617951+Commodoreprime@users.noreply.github.com>
2021-07-06 13:57:58 -0800

    Added bulk text to the readme file and added key bootstrap script

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # skyrimse-linux-steam-mod-bootstraper
 Implements mod loading for Skyrim Special Edition on linux by using UnionFS and Mod Organizer 2 in order to launch with mods in a more flexible way
+
+## Installation
+This script can be ran from anywhere but for ease of use, it can be placed inside of Skyrim's game directory.
+
+Which can be found by going to `Properties...>Local Files>Browse...`
+
+*This readme assumes that you have already gotten moddable Skyrim working with Proton*
+
+## Script requirements
+* Steam (with Steam Play enabled)
+* Skyrim Special Edition (Steam version)
+* [protontricks](https://github.com/Matoking/protontricks)
+* UnionFS (for most distributions this is going to be packaged as `unionfs-fuse`)
+* standard Linux utilities
+
+## Usage
+This script can be ran at any time, the method of for when it is executed is up to you.
+
+One solution is to use the launch options in Steam to launch the script before the game launches (and after for cleanup).
+
+The way to do this is again up to you, my solution is to use a basic menu to wrapper launch with
+
+## Expectations
+This is not a mod organizer, but is, in fact, a bootstrapper.
+
+This script relies on a mod organizer and other tools such as LOOT to compile a correct load order and to store uncompressed mods.
+Basicly do everything except launch the game.
+
+This script runs off of the structure of a portable instance of [Mod Organizer 2](https://github.com/ModOrganizer2/modorganizer)
+where all data is under `C:\\Modding\\MO2`.
+
---
+++ skyrimse-mod-bootstraper.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Implements a unified file system for modding Skyrim Special Edition
+
+helpfile="skyrimse-mod-bootstraper [--decouple]
+Utilizes UnionFS to combine multiple mod directories into a single Data folder
+Options:
+  --decouple    Undo steps taken by the behavior of the bootstrapper
+  --help        Prints this then quits
+"
+[ "$1" = "--help" ] && {
+  printf "$helpfile"
+  exit 0
+}
+
+# Fetch runtime directories
+STEAM_APPID="489830"
+GAME_FILE_DIR="$(protontricks -c "sh -c 'echo -e \$PWD'" "$STEAM_APPID")"
+WINE_PREFIX_DIR="$(protontricks -c "sh -c 'echo -e \$WINEPREFIX'" "$STEAM_APPID")"
+
+APPDATA_DIR="$WINE_PREFIX_DIR/drive_c/users/steamuser/Local Settings/Application Data/Skyrim Special Edition"
+
+# Move into Game directory
+cd "$GAME_FILE_DIR"
+
+case "$1" in
+  "--decouple")
+    /usr/bin/fusermount -u "./Data" 2>/dev/null
+    if [ -d ./Data.base ]; then
+      if [ "$(/usr/bin/ls -l ./Data | grep -v '^total' | wc -l)" -eq 0 ]; then
+        mv Data.base/* ./Data
+        rm -r Data.base
+    fi; fi
+    if [ -L "$APPDATA_DIR" ]; then 
+      rm "$APPDATA_DIR"
+      mv "$APPDATA_DIR.old" "$APPDATA_DIR"
+    fi
+    exit 0 ;;
+  *);;
+esac
+
+# Mod Organizer 2
+#TODO: Maybe make these dynamic?
+MO2_DIR="$WINE_PREFIX_DIR/drive_c/Modding/MO2"
+MO2_MODS_DIR="$MO2_DIR/mods"
+MO2_PROFILE="Default"
+
+MODLIST_FILE="$MO2_DIR/profiles/$MO2_PROFILE/modlist.txt"
+#LOADORDER_FILE="$MO2_DIR/profiles/$MO2_PROFILE/loadorder.txt"
+
+# UnionFS command arguments
+UFS_BRANCH_STRING="Data.base/"
+
+# Append only enabled mods
+while read -r modname; do
+  UFS_BRANCH_STRING="$UFS_BRANCH_STRING:$MO2_MODS_DIR/$modname/"
+done < <(cat "$MODLIST_FILE" | grep '^+' | cut -c 2- | tr -d '\015')
+
+# Move original Data directory to new .base varient so unionfs does not freak out
+mv Data ./Data.base
+
+# unionfs freaks out if there is no directory to mount to
+mkdir ./Data
+
+/usr/bin/unionfs "$UFS_BRANCH_STRING" ./Data
+
+[ -d "$APPDATA_DIR" ] && mv "$APPDATA_DIR" "$APPDATA_DIR.old"
+/usr/bin/ln -srnf "$MO2_DIR/profiles/$MO2_PROFILE" "$APPDATA_DIR"
+
+exit 0
